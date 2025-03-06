23ac29da1dd66fac7ea5b9fd48402747235f6490
Jatin <jatinkrpandit40@gmail.com>
2025-03-06 14:31:56 +0530

    added get and post from dbase

--- src/app.js
+++ src/app.js
@@ -1,37 +1,40 @@
-const express = require('express');
-const connectdb = require('./config/database');
+const express = require("express");
+const connectdb = require("./config/database");
 const app = express();
-const User = require('./models/user');
+const User = require("./models/user");
 
-app.post("/signup",async (req,res)=>{
-const userObj ={
-    firstName: 'Anuj',
-    lastName:'Kumar',
-    emailId:'anuj@gmail.com',
-    password:'anuj12345'
-}
-const user = new User(userObj);
-try {
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
+  try {
     await user.save();
-res.send('User added succesfully!!');
-} catch (error) {
-    res.status(400).send("error saving in dbase:"+error.message);
-}
+    res.send("User added succesfully!!");
+  } catch (error) {
+    res.status(400).send("error saving in dbase:" + error.message);
+  }
 });
 
-
-
-
-
-
-
-
-connectdb().then(()=>{
-    console.log('connection successfull');  
-    app.listen(7777,()=>{
-        console.log('listening bro');
-    });  
-})
-.catch((err)=>{
-console.log(err);
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.find({});
+    if (user.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(user);
+    }
+  } catch (error) {
+    res.status(400).send("error saving in dbase:" + error.message);
+  }
 });
+
+connectdb()
+  .then(() => {
+    console.log("connection successfull");
+    app.listen(7777, () => {
+      console.log("listening bro");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
